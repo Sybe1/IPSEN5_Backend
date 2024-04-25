81ff974dc365d9c45e53a00491daf4c106dbeb61
Sybe Aerts <s1144856@student.hsleiden.nl>
2024-04-25 22:55:05 +0200

    FEAT: PUT en DELETE toegevoegd aan category

--- src/main/java/ipsen5/controller/CategoryController.java
+++ src/main/java/ipsen5/controller/CategoryController.java
@@ -3,10 +3,12 @@ package ipsen5.controller;
 import ipsen5.dao.CategoryDAO;
 import ipsen5.dto.CategoryDTO;
 import ipsen5.models.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -20,7 +22,7 @@ public class CategoryController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Category>> getAllCategories(){
+    public ResponseEntity<List<Category>> getCategories(){
         return ResponseEntity.ok(this.categoryDAO.getAllCategories());
     }
 
@@ -28,5 +30,17 @@ public class CategoryController {
     public ResponseEntity<String> createCategory(@RequestBody CategoryDTO categoryDTO){
         this.categoryDAO.createCategory(categoryDTO);
         return ResponseEntity.ok("Created a new category named " + categoryDTO.name);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> editCategory(@PathVariable UUID id, @RequestBody CategoryDTO categoryDTO){
+        this.categoryDAO.editCategory(id, categoryDTO);
+        return ResponseEntity.ok("Edited category with id: " + id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCategory(@PathVariable("id") UUID id){
+        this.categoryDAO.deleteCategory(id);
+        return ResponseEntity.ok("delete category with id: " + id);
     }
 }
