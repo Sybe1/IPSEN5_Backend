335e67aed1232df5479ab4e19326ba1815745c9f
Sybe Aerts <s1144856@student.hsleiden.nl>
2024-05-10 15:44:05 +0200

    FEAT: CriteriaValidator Aangemaakt om code in Criteria over validaten niet te herhalen

--- src/main/java/ipsen5/controller/CriteriaController.java
+++ src/main/java/ipsen5/controller/CriteriaController.java
@@ -3,6 +3,7 @@ package ipsen5.controller;
 import ipsen5.dao.CriteriaDAO;
 import ipsen5.dto.CriteriaDTO;
 import ipsen5.models.Criteria;
+import ipsen5.services.CriteriaValidator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,9 +14,11 @@ import java.util.List;
 @RequestMapping("/criteria")
 public class CriteriaController {
     private final CriteriaDAO criteriaDAO;
+    private CriteriaValidator criteriaValidator;
 
-    public CriteriaController(CriteriaDAO criteriaDAO) {
+    public CriteriaController(CriteriaDAO criteriaDAO, CriteriaValidator criteriaValidator) {
         this.criteriaDAO = criteriaDAO;
+        this.criteriaValidator = criteriaValidator;
     }
 
     @GetMapping
@@ -25,12 +28,14 @@ public class CriteriaController {
 
     @PostMapping
     public ResponseEntity<String> createCriteria(@RequestBody CriteriaDTO criteriaDTO){
+        criteriaValidator.criteriaValidations(criteriaDTO);
         this.criteriaDAO.createCriteria(criteriaDTO);
         return ResponseEntity.ok("Created a new Criteria named");
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<String> editCriteria(@PathVariable Long id, @RequestBody CriteriaDTO criteriaDTO){
+        criteriaValidator.criteriaValidations(criteriaDTO);
         this.criteriaDAO.editCriteria(id, criteriaDTO);
         return ResponseEntity.ok("Edited criteria with id: " + id);
     }
