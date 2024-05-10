4ec9cf9550925d397851b3392eb9f3351c1c0b84
Sybe Aerts <s1144856@student.hsleiden.nl>
2024-05-10 16:12:50 +0200

    FEAT: ReactionValidator Aangemaakt om code in Reaction over validaten niet te herhalen

--- src/main/java/ipsen5/controller/ReactionController.java
+++ src/main/java/ipsen5/controller/ReactionController.java
@@ -1,13 +1,10 @@
 package ipsen5.controller;
 
 import ipsen5.dao.ReactionDAO;
-import ipsen5.dto.RatingDTO;
 import ipsen5.dto.ReactionDTO;
-import ipsen5.services.UserInputValidator;
-import org.springframework.http.HttpStatus;
+import ipsen5.services.ReactionValidator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -16,39 +13,21 @@ import java.util.UUID;
 @RequestMapping("/rating")
 public class ReactionController {
     private final ReactionDAO reactionDAO;
-    private UserInputValidator validator;
+    private ReactionValidator validator;
 
-    public ReactionController(ReactionDAO reactionDAO, UserInputValidator validator) {
+    public ReactionController(ReactionDAO reactionDAO, ReactionValidator validator) {
         this.reactionDAO = reactionDAO;
         this.validator = validator;
     }
     @PostMapping
     public ResponseEntity<String> createReaction(@RequestBody ReactionDTO reactionDTO){
-        if (!validator.isNotNull(reactionDTO.text)) {
-            throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST, "No valid grade provided"
-            );
-        }
-        if (!validator.isNotNull(reactionDTO.post_id)) {
-            throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST, "No valid post provided"
-            );
-        }
-        if (!validator.isNotNull(reactionDTO.user_id)) {
-            throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST, "No valid user provided"
-            );
-        }
+        validator.reactionValidations(reactionDTO);
         this.reactionDAO.createReaction(reactionDTO);
         return ResponseEntity.ok("Created a new Rating");
     }
     @PutMapping("/{id}")
     public ResponseEntity<String> editReaction(@PathVariable UUID id, @RequestBody ReactionDTO reactionDTO){
-        if (!validator.isNotNull(reactionDTO.text)) {
-            throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST, "No valid reaction provided"
-            );
-        }
+        validator.reactionValidations(reactionDTO);
         this.reactionDAO.editReaction(id, reactionDTO);
         return ResponseEntity.ok("Edited reaction with id: " + id);
     }
