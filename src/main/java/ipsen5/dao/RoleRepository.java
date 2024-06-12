f898f5068272093713db0b68b5cc877d78879504
Sybe Aerts <s1144856@student.hsleiden.nl>
2024-06-12 13:49:17 +0200

    FEAT: Rights aangepast, zodat je per role de goede rights hebt

--- src/main/java/ipsen5/dao/RoleRepository.java
+++ src/main/java/ipsen5/dao/RoleRepository.java
@@ -4,6 +4,8 @@ import ipsen5.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, UUID> {
 }
