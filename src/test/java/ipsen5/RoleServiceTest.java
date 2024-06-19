77a0633fa530f36cb0934eb9893796220144d234
IvanHolwerda <s1133611@student.hsleiden.nl>
2024-06-19 16:48:29 +0200

    create role test

--- src/test/java/ipsen5/RoleServiceTest.java
+++ src/test/java/ipsen5/RoleServiceTest.java
@@ -87,6 +87,20 @@ public class RoleServiceTest {
         assertTrue(foundRoles.isEmpty());
     }
 
+    @Test
+    public void testCreateRole() {
+        // Given
+        RoleDTO roleDTO = new RoleDTO();
+        roleDTO.setId("123");
+        roleDTO.setName("Admin");
+
+        // When
+        roleService.createRole(roleDTO);
+
+        // Then
+        verify(roleRepository, Mockito.times(1)).save(any(Role.class));
+    }
+
 
 
 
