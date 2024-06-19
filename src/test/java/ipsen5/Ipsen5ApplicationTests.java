7e63e0e52e3b9f801e17375908cbeffdc7c98f27
Nebez Aitoty <s1136284@student.hsleiden.nl>
2024-06-19 22:19:43 +0200

    Added test for rubric getALl en find rubric by id

--- src/test/java/ipsen5/Ipsen5ApplicationTests.java
+++ src/test/java/ipsen5/Ipsen5ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Ipsen5ApplicationTests {
+class   Ipsen5ApplicationTests {
 
     @Test
     void contextLoads() {
