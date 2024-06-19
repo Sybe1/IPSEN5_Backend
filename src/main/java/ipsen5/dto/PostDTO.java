8461ccf7d307b0533d90face1277caf6abac4eb3
Sybe Aerts <s1144856@student.hsleiden.nl>
2024-06-19 22:52:00 +0200

    FIX: getter en setter voor testen

--- src/main/java/ipsen5/dto/PostDTO.java
+++ src/main/java/ipsen5/dto/PostDTO.java
@@ -2,10 +2,14 @@ package ipsen5.dto;
 
 import ipsen5.models.User;
 import ipsen5.models.enums.PrefferedDestination;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Getter
+@Setter
 public class PostDTO {
     public String title;
     public String text;
