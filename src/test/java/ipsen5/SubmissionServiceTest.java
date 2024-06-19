9daecd4571934d72ee74b40dd5e6780d701736a6
IvanHolwerda <s1133611@student.hsleiden.nl>
2024-06-19 20:10:18 +0200

    get submission by userID tests

--- src/test/java/ipsen5/SubmissionServiceTest.java
+++ src/test/java/ipsen5/SubmissionServiceTest.java
@@ -1,6 +1,7 @@
 package ipsen5;
 
 import ipsen5.models.Submission;
+import ipsen5.models.User;
 import ipsen5.repository.RubricRepository;
 import ipsen5.repository.SubmissionRespository;
 import ipsen5.services.SubmissionService;
@@ -29,17 +30,35 @@ public class SubmissionServiceTest {
     private Submission submission2;
     private List submissions;
 
+    private User user1;
+    private User user2;
+
     @BeforeEach
     public void setup(){
         submissionRepository = Mockito.mock(SubmissionRespository.class);
         submissionService = new SubmissionService(submissionRepository, rubricRepository);
+
+        user1 = new User();
+        user2 = new User();
+        user1.setId(UUID.randomUUID());
+        user1.setFirst_name("testUser1");
+        user2.setId(UUID.randomUUID());
+        user2.setFirst_name("testUser2");
+
         submission = new Submission();
         submission.setId(UUID.randomUUID());
         submission.setName("test1");
+        submission.setUser_id(user1);
+        
         submission2 = new Submission();
         submission2.setId(UUID.randomUUID());
         submission2.setName("test2");
+        submission2.setUser_id(user2);
+
         submissions = Arrays.asList(submission, submission2);
+
+
+
 
     }
 
@@ -96,6 +115,39 @@ public class SubmissionServiceTest {
 
         // Verify the repository method was called once
         verify(submissionRepository, times(1)).findById(submission.getId());
+    }
+
+    @Test
+    public void testGetSubmissionByUserId() {
+        // Mock the repository call
+        when(submissionRepository.findAll()).thenReturn(submissions);
+
+        // Call the service method
+        List<Submission> foundSubmissions = submissionService.getSubmissionByUserId(user1.getId());
+
+        // Verify the result
+        assertNotNull(foundSubmissions);
+        assertEquals(1, foundSubmissions.size());
+        assertEquals(submission.getId(), foundSubmissions.get(0).getId());
+
+        // Verify the repository method was called once
+        verify(submissionRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void testGetSubmissionByUserIdNoMatches() {
+        // Mock the repository call
+        when(submissionRepository.findAll()).thenReturn(submissions);
+
+        // Call the service method
+        List<Submission> foundSubmissions = submissionService.getSubmissionByUserId(UUID.randomUUID());
+
+        // Verify the result
+        assertNotNull(foundSubmissions);
+        assertTrue(foundSubmissions.isEmpty());
+
+        // Verify the repository method was called once
+        verify(submissionRepository, times(1)).findAll();
     }
 
 
